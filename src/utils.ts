0655e9f39d39606f586331c04f6b651164bcd623
Glitch18 <atharvakn@gmail.com>
2022-09-14 16:56:54 +0530

    feat: Implement LRU Caching

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
 import { ethers, getEthersProvider } from "forta-agent";
+import { Contract } from "ethers";
+import LRU from "lru-cache";
 import { providers } from "ethers";
 import { daiAbi } from "./abis/dai";
 
@@ -8,30 +10,73 @@ export const TRANSFER_EVENT = "event Transfer(address indexed src, address index
 export const L1_DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
 const L2_DAI_ADDRESS = "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1";
 
-export async function getDaiBalance(escrowAddress: string) {
-  const daiContract = new ethers.Contract(L1_DAI_ADDRESS, daiAbi, getEthersProvider());
+class BridgeInfo {
+  readonly daiAddress: string;
+  readonly daiContract: Contract;
+  readonly provider: providers.Provider;
+  static balanceCache: LRU<string, number> = new LRU<string, number>({ max: 100 });
+  static supplyCache: LRU<string, number> = new LRU<string, number>({ max: 100 });
 
-  let balance: number;
-
-  try {
-    balance = await daiContract.balanceOf(escrowAddress);
-  } catch {
-    throw new Error("Invalid Dai Contract");
+  constructor(daiAddress: string, provider: providers.Provider) {
+    this.provider = provider;
+    this.daiContract = new ethers.Contract(daiAddress, daiAbi, this.provider);
+    this.daiAddress = daiAddress;
   }
 
+  async balanceOf(address: string) {
+    // Meant to be used with L1 to fetch balance of DAI Escrows
+
+    // Check if balance is present in Cache
+    const blockNumber = await this.provider.getBlockNumber();
+    if (BridgeInfo.balanceCache.has(address.concat(blockNumber.toString()))) {
+      return BridgeInfo.balanceCache.get(address.concat(blockNumber.toString())) as number;
+    }
+
+    // Balance not present in Cachce, adding new balance to Cache
+    let balance: number;
+
+    try {
+      balance = await this.daiContract.balanceOf(address);
+    } catch {
+      throw new Error("Invalid Dai Contract");
+    }
+
+    BridgeInfo.balanceCache.set(address.concat(blockNumber.toString()), balance);
+    return balance;
+  }
+
+  async totalSupply() {
+    // Meant to be used with L2 for fetching total Dai balance
+
+    const blockNumber = await this.provider.getBlockNumber();
+    const key = JSON.stringify(this.provider).concat(blockNumber.toString());
+
+    // Check if totalSupply present in supplyCache
+    if (BridgeInfo.supplyCache.has(key)) return BridgeInfo.supplyCache.get(key) as number;
+
+    // Value not present for key, adding
+    let totalSupply: number;
+
+    try {
+      totalSupply = await this.daiContract.totalSupply();
+    } catch {
+      throw new Error("Invalid Dai Contract");
+    }
+
+    BridgeInfo.supplyCache.set(key, totalSupply);
+    return totalSupply;
+  }
+}
+
+export async function getDaiBalance(escrowAddress: string) {
+  const contract = new BridgeInfo(L1_DAI_ADDRESS, getEthersProvider());
+  const balance = await contract.balanceOf(escrowAddress);
   return balance;
 }
 
 export async function getL2TotalSupply(provider: providers.Provider) {
-  const l2DaiContract = new ethers.Contract(L2_DAI_ADDRESS, daiAbi, provider);
-
-  let totalSupply: number;
-
-  try {
-    totalSupply = await l2DaiContract.totalSupply();
-  } catch {
-    throw new Error("Invalid Dai Contract");
-  }
+  const contract = new BridgeInfo(L2_DAI_ADDRESS, provider);
+  const totalSupply = await contract.totalSupply();
 
   return totalSupply;
 }
