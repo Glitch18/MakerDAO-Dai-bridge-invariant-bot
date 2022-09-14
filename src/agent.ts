0190b5c8142a5ad77af4c5b49a8a90d715c69cdc
Glitch18 <atharvakn@gmail.com>
2022-09-14 16:11:02 +0530

    feat: Implement bot to monitor invariant

---
+++ src/agent.ts
@@ -0,0 +1,194 @@
+import { Finding, FindingSeverity, FindingType, HandleTransaction, TransactionEvent } from "forta-agent";
+import { providers } from "ethers";
+import {
+  L1_DAI_ADDRESS,
+  TRANSFER_EVENT,
+  ARBITRUM_L1_ESCROW,
+  getDaiBalance,
+  getL2TotalSupply,
+  OPTIMISM_L1_ESCROW,
+} from "./utils";
+
+const arbitrumProvider = new providers.JsonRpcProvider("https://rpc.ankr.com/arbitrum");
+const optimismProvider = new providers.JsonRpcProvider("https://rpc.ankr.com/optimism");
+
+function provideHandleTransaction(
+  transferEvent: string,
+  optimismL1Escrow: string,
+  arbitrumL1Escrow: string,
+  daiAddress: string
+): HandleTransaction {
+  return async (txEvent: TransactionEvent) => {
+    const findings: Finding[] = [];
+
+    // Filter transactions for L1 Escrow's approve events
+    const bridgeEventTransactions = txEvent.filterLog(transferEvent, daiAddress);
+
+    for (const transferEvent of bridgeEventTransactions) {
+      // Check if captured transfer involves either L1Escrows
+      const { src, dst, wad } = transferEvent.args;
+
+      if (src === optimismL1Escrow) {
+        // Dai withdrawn from Optimism
+        findings.push(
+          Finding.fromObject({
+            name: "Dai withdrawn from Optimism",
+            description: "Dai bridge used to transfer Dai from Optimism to L1",
+            alertId: "OPTSM-WTHDRW-1",
+            severity: FindingSeverity.Info,
+            type: FindingType.Info,
+            metadata: {
+              src: src,
+              dst: dst,
+              amt: wad,
+            },
+          })
+        );
+
+        const opL1DaiCount = await getDaiBalance(optimismL1Escrow);
+        const opL2DaiSupply = await getL2TotalSupply(optimismProvider);
+
+        if (opL1DaiCount < opL2DaiSupply) {
+          findings.push(
+            Finding.fromObject({
+              name: "Optimism Dai bridge invariant violated",
+              description: "Optimism L2 Dai supply exceeds L1 Escrow balance",
+              alertId: "OPTSM-INVRNT-1",
+              severity: FindingSeverity.Critical,
+              type: FindingType.Exploit,
+              metadata: {
+                src: src,
+                dst: dst,
+                amt: wad,
+                opL1DaiCount: opL1DaiCount.toString(),
+                opL2DaiSupply: opL2DaiSupply.toString(),
+              },
+            })
+          );
+        }
+      } else if (src === arbitrumL1Escrow) {
+        // Dai withdrawn from Arbitrum
+        findings.push(
+          Finding.fromObject({
+            name: "Dai withdrawn from Arbitrum",
+            description: "Dai bridge used to transfer Dai from Arbitrum to L1",
+            alertId: "ARBTM-WTHDRW-1",
+            severity: FindingSeverity.Info,
+            type: FindingType.Info,
+            metadata: {
+              src: src,
+              dst: dst,
+              amt: wad,
+            },
+          })
+        );
+
+        const arL1DaiCount = await getDaiBalance(arbitrumL1Escrow);
+        const arL2DaiSupply = await getL2TotalSupply(arbitrumProvider);
+
+        if (arL1DaiCount < arL2DaiSupply) {
+          findings.push(
+            Finding.fromObject({
+              name: "Arbitrum Dai bridge invariant violated",
+              description: "Arbitrum L2 Dai supply exceeds L1 Escrow balance",
+              alertId: "ARBTM-INVRNT-1",
+              severity: FindingSeverity.Critical,
+              type: FindingType.Exploit,
+              metadata: {
+                src: src,
+                dst: dst,
+                amt: wad,
+                opL1DaiCount: arL1DaiCount.toString(),
+                opL2DaiSupply: arL2DaiSupply.toString(),
+              },
+            })
+          );
+        }
+      } else if (dst === optimismL1Escrow) {
+        // Dai deposited to Optimism
+        findings.push(
+          Finding.fromObject({
+            name: "Dai deposited to Optimism",
+            description: "Dai bridge used to transfer Dai from L1 to Optimism",
+            alertId: "OPTSM-DPST-1",
+            severity: FindingSeverity.Info,
+            type: FindingType.Info,
+            metadata: {
+              src: src,
+              dst: dst,
+              amt: wad,
+            },
+          })
+        );
+
+        const opL1DaiCount = await getDaiBalance(optimismL1Escrow);
+        const opL2DaiSupply = await getL2TotalSupply(optimismProvider);
+
+        if (opL1DaiCount < opL2DaiSupply) {
+          findings.push(
+            Finding.fromObject({
+              name: "Optimism Dai bridge invariant violated",
+              description: "Optimism L2 Dai supply exceeds L1 Escrow balance",
+              alertId: "OPTSM-INVRNT-1",
+              severity: FindingSeverity.Critical,
+              type: FindingType.Exploit,
+              metadata: {
+                src: src,
+                dst: dst,
+                amt: wad,
+                opL1DaiCount: opL1DaiCount.toString(),
+                opL2DaiSupply: opL2DaiSupply.toString(),
+              },
+            })
+          );
+        }
+      } else if (dst == arbitrumL1Escrow) {
+        // Dai deposited to Arbitrum
+        findings.push(
+          Finding.fromObject({
+            name: "Dai deposited to Arbitrum",
+            description: "Dai bridge used to transfer Dai from L1 to Arbitrum",
+            alertId: "ARBTM-DPST-1",
+            severity: FindingSeverity.Info,
+            type: FindingType.Info,
+            metadata: {
+              src: src,
+              dst: dst,
+              amt: wad,
+            },
+          })
+        );
+
+        const arL1DaiCount = await getDaiBalance(arbitrumL1Escrow);
+        const arL2DaiSupply = await getL2TotalSupply(arbitrumProvider);
+
+        if (arL1DaiCount < arL2DaiSupply) {
+          findings.push(
+            Finding.fromObject({
+              name: "Arbitrum Dai bridge invariant violated",
+              description: "Arbitrum L2 Dai supply exceeds L1 Escrow balance",
+              alertId: "ARBTM-INVRNT-1",
+              severity: FindingSeverity.Critical,
+              type: FindingType.Exploit,
+              metadata: {
+                src: src,
+                dst: dst,
+                amt: wad,
+                opL1DaiCount: arL1DaiCount.toString(),
+                opL2DaiSupply: arL2DaiSupply.toString(),
+              },
+            })
+          );
+        }
+      } else {
+        continue;
+      }
+    }
+
+    return findings;
+  };
+}
+
+export default {
+  handleTransaction: provideHandleTransaction(TRANSFER_EVENT, OPTIMISM_L1_ESCROW, ARBITRUM_L1_ESCROW, L1_DAI_ADDRESS),
+};
---
+++ src/utils.ts
@@ -0,0 +1,37 @@
+import { ethers, getEthersProvider, LogDescription } from "forta-agent";
+import { providers } from "ethers";
+import { daiAbi } from "./abis/dai";
+
+export const OPTIMISM_L1_ESCROW = "0x467194771dAe2967Aef3ECbEDD3Bf9a310C76C65";
+export const ARBITRUM_L1_ESCROW = "0xA10c7CE4b876998858b1a9E12b10092229539400";
+export const TRANSFER_EVENT = "event Transfer(address indexed src, address indexed dst, uint wad)";
+export const L1_DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+const L2_DAI_ADDRESS = "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1";
+
+export async function getDaiBalance(escrowAddress: string) {
+  const daiContract = new ethers.Contract(L1_DAI_ADDRESS, daiAbi, getEthersProvider());
+
+  let balance: number;
+
+  try {
+    balance = await daiContract.balanceOf(escrowAddress);
+  } catch {
+    throw new Error("Invalid Dai Contract");
+  }
+
+  return balance;
+}
+
+export async function getL2TotalSupply(provider: providers.Provider) {
+  const l2DaiContract = new ethers.Contract(L2_DAI_ADDRESS, daiAbi, provider);
+
+  let totalSupply: number;
+
+  try {
+    totalSupply = await l2DaiContract.totalSupply();
+  } catch {
+    throw new Error("Invalid Dai Contract");
+  }
+
+  return totalSupply;
+}
